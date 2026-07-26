138897015af773316eb06f4681749f2e79172ff0
cyxigo <cyxigo@proton.me>
2026-07-26 15:59:06 +0300

    Bump version to `v4.0.3`

--- src/include/wi_conf.h
+++ src/include/wi_conf.h
@@ -7,7 +7,7 @@
 /**
  * Wi version as a string
  */
-#define WI_VERSION_STRING "4.0.2-beta"
+#define WI_VERSION_STRING "4.0.3-beta"
 
 enum {
     /**
@@ -15,7 +15,7 @@ enum {
      */
     WI_VERSION_MAJOR = 4,
     WI_VERSION_MINOR = 0,
-    WI_VERSION_PATCH = 2,
+    WI_VERSION_PATCH = 3,
 
     /**
      * Compiler limits
