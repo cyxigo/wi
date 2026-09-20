7e8a469cb790051e54abcf5ea35bf64d330213f2
cyxigo <cyxigo@proton.me>
2026-09-20 09:08:53 +0300

    Add test/lang/operator
    Some of these will probably repeat once I add test/lang/object

--- util/test.py
+++ util/test.py
@@ -3,24 +3,32 @@ from pathlib import Path
 from subprocess import run
 
 # runs every test/bug and test/lang test and compares its output to the respective .wiwi file
-# first line of every .wi script must be either $ok (compare stdout) or $fail (compare stderr)
+# first line of every .wi script must be either $ok (compare stdout), $fail (compare stderr), or $skip to skip
 # might be pretty silly but works nonetheless
 #
 # why .wiwi? because .out is git-ignored and idk i couldn't come up with something better
 wi = "wi"
+exit_code = 0
+passed = 0
 failed = 0
+skipped = 0
 
 
 def test(path, recursive=False):
     path = Path(path)
     scripts = path.rglob("*.wi") if recursive else path.glob("*.wi")
 
+    global exit_code
+    global passed
+    global failed
+
     for script in sorted(scripts):
         out = script.with_suffix(".wiwi")
 
-        if not out.is_file():
-            print(f"[SKIP] {script} has no respective .wiwi file")
-            continue
+        def skip(msg):
+            print(f"[SKIP] {script} {msg}")
+            global skipped
+            skipped += 1
 
         kind = script.read_text().splitlines()[0]
         stream = None
@@ -29,8 +37,15 @@ def test(path, recursive=False):
             stream = "stdout"
         elif kind == "// $fail":
             stream = "stderr"
+        elif kind == "// $skip":
+            skip("$skip")
+            continue
         else:
-            print(f"[SKIP] {script} kind was not set")
+            skip("kind was not set")
+            continue
+
+        if not out.is_file():
+            skip("has no respective .wiwi file")
             continue
 
         result = run([wi, script], capture_output=True, text=True)
@@ -41,13 +56,15 @@ def test(path, recursive=False):
             print(f"[FAIL] {script} has unexpected output!")
             print(f"--- expected ---\n{expected}")
             print(f"--- actual ---\n{actual}")
-            global failed
-            failed = 1
+
+            exit_code = 1
+            failed += 1
         else:
             print(f"[PASS] {script} passed!")
+            passed += 1
 
 
 test("./test/bug")
 test("./test/lang", recursive=True)
-
-exit(failed)
+print(f"[RESULT] {passed} passed, {failed} failed, {skipped} skipped")
+exit(exit_code)
